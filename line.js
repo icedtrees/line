9c3e47910ba555c38e8695b7332582c13ad3785f
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2017-01-03 21:48:56 +1100

    Add basic content script

---
+++ line.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "red";
+console.log("asdf");
+elements = document.getElementsByClassName("_51sy");
+  for (var i = 0; i < elements.length; i++) {
+  elements[i].textContent = "Im a cat";
+}
